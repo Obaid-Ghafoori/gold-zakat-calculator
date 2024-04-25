f87689fb23660a9ba912def36e6b2b3bd42d6695
Obaid Ghafoori <obaid.ghafoori9@gmail.com>
2024-04-25 03:21:40 +0200

    refactor: clean up

--- gold-zakat-calculator/app/calculate/page.tsx
+++ gold-zakat-calculator/app/calculate/page.tsx
@@ -74,94 +74,94 @@ const ZakatCalculatorPage = () => {
 
     const { zakatDetails, totalZakat } = calculateZakat();
     const resultText =
-      `Zakat Details:\n` +
+      `The total zakat to pay in grams overview:\n` +
       zakatDetails
         .map(
           (detail) =>
-            `Grams: ${detail.grams}, Karat: ${detail.karat
-            }, Zakat Payable: ${detail.zakatPayable.toFixed(4)}\n`
+            `Grams: ${detail.grams}  \t-\t Karat: ${detail.karat
+            }  \t- \tZakat Payable: ${detail.zakatPayable.toFixed(4)}g\n`
         )
-        .join("") +
-      `Total Zakat Payable: ${totalZakat.toFixed(4)}`;
+        .join("") +'------------------------------------------------------\n'+
+      `Total Zakat Payable: \t\t\t\t\t\t\t\t\t\t${totalZakat.toFixed(4)}g`;
     setZakatResult(resultText);
   };
 
   return (
     <>
-      <NavBar/>
-      <div className="container flex flex-col items-center justify-center text-center px-4 sm:px-6 lg:px-8">
-        {/* <h1 className="text-center text-2xl sm:text-3xl md:text-4xl lg:text-5xl mt-4 mb-2">Zakat Calculator</h1> */}
-        <p>
-          Welcome to the Zakat Calculator. Please add your gold details below to
-          calculate the Zakat payable.
-        </p>
-        {goldItems.map((item, index) => (
-          <div key={index} className="gold-item text-white">
-            <div className="flex items-center">
-              {/* <label htmlFor={`grams-${index}`}>Grams:</label> */}
-              <input
-                className="input input-bordered input-success input-xs max-w-xs text-white m-3"
-                placeholder="Provide gold quantity"
-                type="number"
-                id={`grams-${index}`}
-                name="grams"
-                value={item.grams}
-                onChange={(event) => handleInputChange(event, index)}
-              />
-              {/* <label htmlFor={`karat-${index}`}>Karat:</label> */}
-              <input
-                className="input input-bordered input-success input-xs max-w-xs text-white m-3"
-                placeholder="Select gold type"
-                type="number"
-                id={`karat-${index}`}
-                name="karat"
-                value={item.karat}
-                onChange={(event) => handleInputChange(event, index)}
-              />
-              <button
-                className="btn btn-bordered btn-success btn-square btn-outline btn-xs tooltip tooltip-right flex justify-center items-center"
-                data-tip="Add more gold"
-                onClick={handleAddGold}
+    <NavBar/>
+    <div className="container flex flex-col items-center justify-center text-center">
+      <h1 className="text-center">Zakat Calculator</h1>
+      <p>
+        Welcome to the Zakat Calculator. Please add your gold details below to
+        calculate the Zakat payable.
+      </p>
+      {goldItems.map((item, index) => (
+        <div key={index} className="gold-item text-white">
+          <div className="flex items-center">
+            <label htmlFor={`grams-${index}`}>Grams:</label>
+            <input
+              className="input input-bordered input-success input-xs max-w-xs text-white m-3"
+              placeholder="Provide gold quantity"
+              type="number"
+              id={`grams-${index}`}
+              name="grams"
+              value={item.grams}
+              onChange={(event) => handleInputChange(event, index)}
+            />
+            <label htmlFor={`karat-${index}`}>Karat:</label>
+            <input
+              className="input input-bordered input-success input-xs max-w-xs text-white m-3"
+              placeholder="Select gold type"
+              type="number"
+              id={`karat-${index}`}
+              name="karat"
+              value={item.karat}
+              onChange={(event) => handleInputChange(event, index)}
+            />
+            <button
+              className="btn btn-bordered btn-success btn-square btn-outline btn-xs tooltip tooltip-right flex justify-center items-center"
+              data-tip="Add more gold"
+              onClick={handleAddGold}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-                  />
-                </svg>
-              </button>
-            </div>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                />
+              </svg>
+            </button>
           </div>
-        ))}
-        <textarea
-          className="textarea textarea-success w-full max-w-sm"
-          placeholder="Zakat Calculation Results"
-          value={zakatResult}
-          readOnly
-        ></textarea>
+        </div>
+      ))}
+      <textarea
+        className="textarea textarea-success fix w-full max-w-sm first-line:font-light"
+        placeholder="Zakat Calculation Results"
+        value={zakatResult}
+        readOnly
+      ></textarea>
 
-        <button
-          className="btn btn-primary w-64 rounded-full btn-sm m-3"
-          onClick={handleSubmit}
-        >
-          Calculate Zakat
-        </button>
-        <Link
-          className="btn btn-outline btn-info w-64 rounded-full btn-sm"
-          href="/"
-        >
-          Go Back to Home
-        </Link>
-      </div>
-    </>
+      <button
+        className="btn btn-primary w-64 rounded-full btn-sm m-3"
+        onClick={handleSubmit}
+      >
+        Calculate Zakat
+      </button>
+      <Link
+        className="btn btn-outline btn-info w-64 rounded-full btn-sm"
+        href="/"
+      >
+        Go Back to Home
+      </Link>
+    </div></>
+    
   );
 };
 
